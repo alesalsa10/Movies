24b76ab048358404fd3a122252d728e2d2b0229f
alesalsa10 <alexandersalsa10@gmail.com>
2020-07-26 17:47:01 -0400

    changed app.listen

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const axios = require('axios').default;
 const app = express();
-app.listen(3000);
+app.listen(process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
