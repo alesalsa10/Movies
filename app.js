16d439ce4dc05fab3cc629ff65e6fc001ae4ac60
alesalsa10 <alexandersalsa10@gmail.com>
2020-08-06 13:39:09 -0400

    added trending

--- app.js
+++ app.js
@@ -4,41 +4,58 @@ const express = require('express');
 const axios = require('axios').default;
 const app = express();
 app.listen(process.env.PORT);
+//app.listen(3000);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-    res.render('search')
-})
+app.get('/', function (req, res) {
+    //res.render('search')
+    let fetchData = async () => {
+        const res = await axios.get(`https://api.themoviedb.org/3/trending/tv/week?api_key=f2e1db77c8ae74a5c21ae7b7d5630dfb`);
 
-app.get('/results', function(req, res){
+        const tvData = res['data'];
+        const res2 = await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=f2e1db77c8ae74a5c21ae7b7d5630dfb`);
+
+        const movieData = res2['data'];
+        return [tvData, movieData];
+    }
+
+    (async () => {
+        allData = await fetchData();
+        tvData = allData[0];
+        movieData = allData[1];
+        res.render('search', {tvData: tvData, movieData:movieData});
+    })()
+});
+
+app.get('/results', function (req, res) {
     //value of input text field with the name of search
     let search = req.query.search;
     axios.get(`http://www.omdbapi.com/?s=${search}${apiKey}`)
         .then(function (response) {
-        // handle success
-        let data = response['data'];
-        res.render('results', {data: data});
-    })
+            // handle success
+            let data = response['data'];
+            res.render('results', { data: data });
+        })
         .catch(function (error) {
-        // handle error
-        console.log(error);
-    });
+            // handle error
+            console.log(error);
+        });
 });
 
-app.get('/info/:name', function(req, res){
+app.get('/info/:name', function (req, res) {
     //value of input text field with the name of title
     let name = req.params.name;
     axios.get(`http://www.omdbapi.com/?t=${name}${apiKey}`)
         .then(function (response) {
-        // handle success
-        let data = response['data'];
-        res.render('info', {data: data});
-    })
+            // handle success
+            let data = response['data'];
+            res.render('info', { data: data });
+        })
         .catch(function (error) {
-        // handle error
-        console.log(error);
-    });
-});
+            // handle error
+            console.log(error);
+        });
+})
